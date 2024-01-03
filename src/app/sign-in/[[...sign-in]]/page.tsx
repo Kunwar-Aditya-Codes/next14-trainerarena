c80575f6aa84e6e86cfc7401e3cce12b29310f3f
Kunwar Aditya <adityakunwar0147@gmail.com>
2024-01-04 00:36:05 +0530

    initial commit

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,17 @@
+import { SignIn } from '@clerk/nextjs';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <div className='h-screen flex flex-col items-center justify-center gap-y-8'>
+      <Image
+        src='/main-logo.png'
+        alt='logo'
+        className='w-[6.3rem] h-24 shadow-md shadow-white/5 rounded-full p-3 '
+        height={500}
+        width={500}
+      />
+      <SignIn />
+    </div>
+  );
+}
---
+++ src/components/all/Navbar.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import MaxWidthWrapper from './MaxWidthWrapper';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className='shadow-sm sticky top-0 py-3 border-b-muted bg-background border-b'>
+      <MaxWidthWrapper>
+        <div className='flex items-center justify-between'>
+          <Link href={'/'}>
+            <Image
+              src={'/main-logo.png'}
+              alt='logo'
+              width={500}
+              height={500}
+              className='w-[3.3rem] h-12 rounded-full p-1'
+            />
+          </Link>
+
+          <div>
+            <Button className='rounded-full'>
+              <Link href={'/sign-in'}>Sign In</Link>
+            </Button>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </div>
+  );
+};
+
+export default Navbar;
